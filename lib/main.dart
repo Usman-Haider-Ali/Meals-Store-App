28e6b94a87c98e3cf843e448b2e9e9c6a5a8eb58
Usman Haider Ali <theUsmanHaider@gmail.com>
2022-08-10 19:17:12 +0500

    Created BottomNavigation Menu and its item

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import './screens/tabs_screen.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Meals Store'),
-        ),
-        body: const Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return TabsScreen();
   }
 }
